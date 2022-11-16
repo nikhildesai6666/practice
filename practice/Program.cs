adb0756e51ef3f6030c67184ae0253a2b945173d
nikhildesai6666 <desainikhil>
2022-11-16 11:04:56 +1030

    Add project files.

---
+++ practice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter no");
+            
+            
+
+            
+
+
+        }
+    }
+}
